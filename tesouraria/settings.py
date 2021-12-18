057fb52c1458c4015acae7bc566327137fac215d
J. A. Matioli <j.a.matioli@gmail.com>
2021-12-18 12:15:03 -0300

    Deploy to Heroku

--- tesouraria/settings.py
+++ tesouraria/settings.py
@@ -43,7 +43,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'django.contrib.humanize',
-    'bootstrapform',
+    'crispy_forms',
     'apps.categoria',
     'apps.conta',
     'apps.movimento',
@@ -128,11 +128,18 @@ NUMBER_GROUPING = 3
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = 'static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+if DEBUG:
+    STATICFILES_DIRS = [
+        os.path.join(BASE_DIR, 'static')
+    ]
+else:
+    STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-django_heroku.settings(locals())
+#django_heroku.settings(locals())
+
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
