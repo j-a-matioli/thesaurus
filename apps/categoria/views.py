b730eb3c0857eda6d5a6bb95b272a90d59b2c63a
Prof. Matioli <matioli@cotil.unicamp.br>
2021-12-20 11:27:48 -0300

    tentando resolver erro 500

--- apps/categoria/views.py
+++ apps/categoria/views.py
@@ -26,9 +26,10 @@ class CategoriaDelete(DeleteView):
     model = Categoria
 
     def delete(self,*args, **kwargs):
+        return HttpResponseRedirect("<h1>Olaaaaaaaaaaaaaaaaaaaaaaaa</h1>")
         self.object = self.get_object()
-        success_url = 'categoria/' #reverse_lazy('list_categoria')
-        fail_url = 'denied/' #reverse_lazy('denied_delete')
+        success_url = reverse_lazy('list_categoria')
+        fail_url = reverse_lazy('denied_delete')
         if self.object.conta_set.exists():
             return HttpResponseRedirect(fail_url)
 
