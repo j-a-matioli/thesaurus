461a123d34ebddf57e85ca48b9e6f3926a8528da
Prof. Matioli <matioli@cotil.unicamp.br>
2021-12-20 10:54:32 -0300

    tentando resolver erro 500

--- apps/categoria/urls.py
+++ apps/categoria/urls.py
@@ -3,7 +3,7 @@ from django.urls import path, include
 from apps.categoria.views import CategoriaList, CategoriaCreate, CategoriaUpdate, CategoriaDelete,DeniedDeleteCategoria
 
 urlpatterns = [
-    path('', CategoriaList.as_view(), name='list_categoria'),
+    path('/', CategoriaList.as_view(), name='list_categoria'),
     path('denied/', DeniedDeleteCategoria, name='denied_delete'),
     path('create/', CategoriaCreate.as_view(), name='create_categoria'),
     path('update/<int:pk>', CategoriaUpdate.as_view(), name='update_categoria'),
