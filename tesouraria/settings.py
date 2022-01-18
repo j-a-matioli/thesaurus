7e6efc5a2d64ce71d2613cb428050e11db7c61a6
Jose A. Matioli <j.a.matioli@gmail.com>
2022-01-18 18:02:18 -0300

    Tentando deploiar no oceano digital

--- tesouraria/settings.py
+++ tesouraria/settings.py
@@ -4,6 +4,7 @@ from decouple import config
 from pathlib import Path
 from dj_database_url import parse as db_url
  
+DISABLE_COLLECTSTATIC=1
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
