9813fae0d75ee48fa2aaecd746aef413c446eac6
J. A. Matioli <j.a.matioli@gmail.com>
2022-01-19 10:59:34 -0300

    Continuo tentando deploiar no oceano digital

--- tesouraria/settings.py
+++ tesouraria/settings.py
@@ -131,7 +131,7 @@ NUMBER_GROUPING = 3
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
-STATIC_URL = "static/"
+STATIC_URL = os.path.join(BASE_DIR, "static") # "static/"
 STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 
 # Default primary key field type
