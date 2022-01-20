92c350723f45ee0f0f7a814d49a4ea64aacd0d00
J. A. Matioli <j.a.matioli@gmail.com>
2022-01-20 00:59:08 -0300

    Continuo tentando deploiar no oceano digital

--- apps/conta/urls.py
+++ apps/conta/urls.py
@@ -4,7 +4,7 @@ from apps.conta.views import ContaList, ContaCreate, ContaUpdate, ContaDelete
 
 urlpatterns = [
     path('', ContaList.as_view(), name='list_conta'),
-    path('create', ContaCreate.as_view(), name='create_conta'),
+    path('create/', ContaCreate.as_view(), name='create_conta'),
     path('update/<int:pk>', ContaUpdate.as_view(), name='update_conta'),
     path('delete/<int:pk>', ContaDelete.as_view(), name='delete_conta'),
 ]
